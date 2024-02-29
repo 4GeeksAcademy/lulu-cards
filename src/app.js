45b18287d85c7ebeb72e1ba1f827d3cb55874c95
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-02-29 12:51:13 +0000

    needs htlm

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardsValues = ["A",2", "3", "4","6","7","J","Q","K"] 
+  let cardsTypes = ["♦", "♥", "♠", "♣"]
+  let cardsValueElement = document.getElementById("cardValue");
+  let cardsTypeTopElement = document.getElementById("cardTypeTop");
+  let cardsTypeBottomElement = document.getElementById("cardTypeBottom");
+
+  function getRandomCard() {
+    let randomValueIndex = Math.floor(math.random() * cardsValues.length); 
+    let randomValue =cardsValues[randomValueIndex];
+    cardsTypesTopElement.textContent = randomTypes;
+    cardsTypeBottomElement.textContent = randomTypes;
+    cardValueelement.textContent = randomValue;
+
+    if (randomTypes == "♦") {
+      cardsTypeTopElement.style.color = "red";
+      
+    }
+  
+  }
+
 };
